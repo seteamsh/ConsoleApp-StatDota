e7ec661d75965fc1675c16ecd18f9c0731adf205
seteamsh <seteamsh@gmail.com>
2025-03-07 23:20:14 +0500

    realize addFavorites and show favorites

--- ConsoleApp StatDota/ConsoleApp StatDota/Main.swift
+++ ConsoleApp StatDota/ConsoleApp StatDota/Main.swift
@@ -2,13 +2,24 @@ import Darwin
 var favorites = [Player]()
 func addFavorite(player: Player) {
     favorites.append(player)
+    print("Игрок с ID \(player.id) добавлен в избранное")
 }
 func searchID() {
-    var foundPlayer: Player?
-    print("Введите ID игрока:")
-    let id = readLine() ?? ""
-    foundPlayer = findPlayerByID(Int(id) ?? 0)
-    print(foundPlayer)
+        var foundPlayer: Player?
+        var action: String
+        print("Введите ID игрока:")
+        let id = readLine() ?? ""
+        foundPlayer = findPlayerByID(Int(id) ?? 0)
+        print(foundPlayer)
+        
+        print("1. Добавить в избранное")
+        print("2. Выход в главное меню")
+        action = readLine() ?? ""
+        switch action {
+        case "1": addFavorite(player: foundPlayer!)
+        case "2": break
+        default: print("Ввели неверную команду, повторите снова")
+        }
 }
 
 func findPlayerByID(_ id: Int) -> Player? {
@@ -22,15 +33,18 @@ func findPlayerByID(_ id: Int) -> Player? {
     return nil
 }
 
+func showFavorites() {
+    print("\(favorites)")
+}
+
 
 @main
 struct Main {
     static func main() throws {
         while true {
             print("Добро пожаловать в StatDota")
-            print("Здесь вы сможете получить статистику по игрокам")
-            print("-----------------------------------------------")
-            print("Выберите команду, чтобы начать")
+            print("---------------------------")
+            print("Выберите команду")
             print("")
             print("1. Поиск игрока по ID")
             print("2. Сохраненные игроки")
@@ -40,7 +54,7 @@ struct Main {
             case "1":
                 searchID()
             case "2":
-                print("Сохраненные игроки")
+                showFavorites()
             case "3":
                 exit(0)
             default :
