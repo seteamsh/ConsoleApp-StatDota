b615e9505f55661df03f73225fe615ffb8a6c4b3
seteamsh <seteamsh@gmail.com>
2025-03-12 22:48:01 +0500

    try refactoring

--- ConsoleApp StatDota/ConsoleApp StatDota/Application.swift
+++ ConsoleApp StatDota/ConsoleApp StatDota/Application.swift
@@ -1,11 +1,17 @@
 class Application {
-    let actions = [SearchPlayer(), Favorites(), Exit()]
     func run() {
+        print("Добро пожаловать в StatDota")
         while true {
-            print("Добро пожаловать в StatDota")
+            let searchPlayer = SearchPlayer()
+            let favorites = Favorites()
+            print("1. Поиск игрока по ID")
+            print("2. Избранные")
+            print("3. Выход")
             let getAction = Actions.getDataFromUser("Введите номер действия:")
-            for action in actions {
-                action.run()
+            switch getAction {
+            case "1": searchPlayer.run()
+            case "2": favorites.showFavorites()
+            default : break
             }
         }
     }
--- ConsoleApp StatDota/ConsoleApp StatDota/SearchPlayer.swift
+++ ConsoleApp StatDota/ConsoleApp StatDota/SearchPlayer.swift
@@ -4,18 +4,19 @@ class SearchPlayer: Actions {
     }
     func searchID() {
         var foundPlayer: Player?
-        let id = getDataFromUser("Введите ID игрока:")
+        var favorites = Favorites()
+        let id = Actions.getDataFromUser("Введите ID игрока:")
         foundPlayer = findPlayerByID(Int(id) ?? 0)
         print(foundPlayer)
         
         print("1. Добавить в избранное")
         print("2. Поиск игрока по ID")
         print("3. Выход в главное меню")
-        let action = getDataFromUser("Выберите команду:")
+        let action = Actions.getDataFromUser("Выберите команду:")
         switch action {
-        //case "1": addFavorite(player: foundPlayer!)
+        case "1": favorites.addFavorite(player: foundPlayer!)
         case "2": searchID()
-        case "3": break
+        case "3": return
         default: print("Ввели неверную команду, повторите снова")
         }
     }
