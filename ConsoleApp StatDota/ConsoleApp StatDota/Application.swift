c33e37577d72db2b2aad29cb3ab7206360030d2e
seteamsh <seteamsh@gmail.com>
2025-03-13 20:41:29 +0500

    mistake don't work favorites

--- ConsoleApp StatDota/ConsoleApp StatDota/Application.swift
+++ ConsoleApp StatDota/ConsoleApp StatDota/Application.swift
@@ -1,9 +1,12 @@
 class Application {
+    let searchPlayer = SearchPlayer()
+    let favorites = Favorites()
+    let exit = Exit()
     func run() {
+        
         print("Добро пожаловать в StatDota")
         while true {
-            let searchPlayer = SearchPlayer()
-            let favorites = Favorites()
+            
             print("1. Поиск игрока по ID")
             print("2. Избранные")
             print("3. Выход")
@@ -11,7 +14,7 @@ class Application {
             switch getAction {
             case "1": searchPlayer.run()
             case "2": favorites.showFavorites()
-            default : break
+            default : exit.exit()
             }
         }
     }
